b4e81d561c9bb1355cd79115ef396454d0bd0d82
James Fleming <jamesfleming@jamess-mbp-9.lan>
2020-11-06 12:58:54 -0500

    Finished transition model

--- RobotModel.py
+++ RobotModel.py
@@ -5,14 +5,15 @@ from Maze import Maze
 
 class RobotModel(model):
     def __init__(self, moves, mazeFileName):
-        self.transitionMatrix = self.createTransitionModel()
+        # Other methods use the maze so make sure it is declared first. 
         self.maze = Maze(mazeFileName)
+        self.transitionMatrix = self.createTransitionModel()
         self.prior = self.createPrior()
-        self.updateMatrixDictionary = self.createUpdateDictionary
-        super().__init__(self.prior, self.transitionMatrix)
+        self.updateMatrixDictionary = self.createUpdateMatrixDictionary()
+        super().__init__(self.prior, self.transitionMatrix, self.updateMatrixDictionary)
 
 
-    # create a uniform distribution of where the robot can be based on knowledge of map
+    # create a uniform distribution of where the robot can be based on knowledge of maze
     def createPrior(self):
         # prior should be a 16x 1 matrix. (n^2 x 1 in the general case)
         # Where each row of the matrix is for a different position in the maze. and the number in column 0 is the probability of being in that position
@@ -38,22 +39,70 @@ class RobotModel(model):
         return prior
 
 
-                
-        # Go through each position of a maze. 
-
-
-
+    # create a transition matrix relating one position in maze to another position.           
     def createTransitionModel(self):
         # a 16 x16 matrix. Where the rows are previous position of robot 
-        # columns are current position of robot. Value at  (1,1 ) would be probability robot was at position 1 and is now at position 1
+        # columns are current position of robot.
+
+        # Create an empty matrix that is all the positions in the maze by all the positions in the maze
+        # how many positions are in the maze?
+        numPositions = self.maze.width * self.maze.height
+        transitionMatrix = np.zeros(numPositions**2)
+        # remember here that the height of a matrix refers to # of rows and width refers to # of columns
+        # we will see numerous instances where indexing into a matrix vs (x,y) positions will be tricky
+        transitionMatrix = transitionMatrix.reshape((numPositions, numPositions))
+
+        #  Value at  (1,1 ) would be probability robot was at position 1 and is now at position 1
+
         # position 1 would obviously refer to a tuple maybbe (1,0) for the x and y coordinate of the robtot
+        
         # Tricky!! We are told that the robot will move with a .25 percent probability in any direction
         # Need to remember that there are walls. So if there is a wall the probability that it doesnt move
-        # becomes nonzero. Probability it doesn't move becomes sum of all the walls around that position. 
+        # becomes nonzero. 
 
-        return []
 
-      def createUpdateMatrixDictionary(self):
+        # Go through every location of the maze. This is our previous position. 
+        # see which neighbors are walls vs floors.  Leave walls as assigned 0, neighbor floors get .25
+        # the same position gets .25*numOfWallNeighbors
+        for x in range(self.maze.width):
+            for y in range(self.maze.height):
+                
+                # Now go through each neighbor of this position if it is not a wall
+                if (self.maze.is_floor(x,y)):
+                    # Simple algebra can convert a position in the maze to its index in the transition matrix 
+                    # Because of how we order the positions of a maze in our matrix equivalent. Current Position (0,0) is row 0 in matrix. Next position of (0,0) would be column 0 in matrix
+                    
+                    rowIndex = x*self.maze.width + y
+                    
+                    #keep track of how many neighbors are walls. 
+                    numWallNeighbors = 0
+
+                    # go through neighbors of this position(up +-1 and right +-1)
+                    for d in ([-1, 1]):
+                        xIndex = (x+d)*self.maze.width + y 
+                        yIndex = x*self.maze.width + (y+d)
+                        if (self.maze.is_floor(x+d, y)):
+                            # the probability that we were at a give (x,y) and transitioned to 
+                            # (x+d, y) is 0.25 if (x+d, y) is a floor
+                            transitionMatrix[rowIndex][xIndex] = 0.25
+                        else:
+                            numWallNeighbors+=1
+                        
+                        if (self.maze.is_floor(x,y+d)):
+                            transitionMatrix[rowIndex][yIndex] = 0.25
+                        else:
+                            numWallNeighbors+=1
+                    # Now we know how many neighbors are walls so we can assign the probability 
+                    # That a robot doesn't actually move to be .25* the number of walls.
+                    # For instance if has 3 walls on 3 sides then 75 percent of the time it tries to move
+                    # it will just bump into a wall and not actually move
+                    transitionMatrix[rowIndex][rowIndex] = .25* numWallNeighbors
+        return transitionMatrix
+
+
+    # create an update matrix that key is color observed a matrix that relates position robot is at to probability key color was observed
+    # 
+    def createUpdateMatrixDictionary(self):
         return {}
         # is based of the sensor model. We are given a color. Then we ask what is the probability we got that color from a specific position. 
         # there are 16 or (n^2) positions that the robot could be. And we need a probability of reading color c for each of those positions
@@ -62,21 +111,39 @@ class RobotModel(model):
         # diagnols correspond to the probailities and every other entry is 0. We need the information of the maze to tell us this because the robot cannot actually get sensor readings.
     
     
-    
-    
-
 
 
 if __name__ == "__main__":
     # Make sure we get a column vector of 16 with all equal probabilities of 1/16 
     # This is the prior of an empty maze
     r1 = RobotModel([], "emptyMaze.maz")
-    print(r1.prior)
-    print(r1.prior.shape)
+    #print(r1.prior)
+    #print(r1.prior.shape)
     # Now add a few walls and makes sure we get equal probabilities of 1/13 for the places with no walls and 0 for places with walls
 
     r1 = RobotModel([], "maze1.maz")
-    print(r1.prior)
-    print(r1.prior.shape)
+    #print(r1.prior)
+    #print(r1.prior.shape)
 
     # Looks good, prior function is complete. 
+    startArray = np.identity(4)
+    #print(startArray[0][0])
+    #print(startArray[3][3])
+    # direct array manipulation (remember that x and y are flipped, it takes row , column)
+    startArray[2][1] = 1
+    #print(startArray[2][1])
+    #print(startArray)
+
+    startArray = np.zeros(4*4)
+    startArray = startArray.reshape((4,4))
+    #print(startArray)
+    #print(startArray.shape)
+    #print(r1.transitionMatrix)
+    #print(r1.transitionMatrix.shape)
+    print(r1.transitionMatrix)
+    # Test an play around with different values in the matrix. 
+    # Pretty confident our transition matrix is correctly built!
+    print(r1.transitionMatrix[10][5])
+
+
+
